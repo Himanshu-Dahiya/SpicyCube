690f1fa4202b35d3831ad2cd4d5d73fa004d4271
Himanshu Dahiya <himanshudahiya215@gmail.com>
2020-05-05 13:52:27 +0530

    Back-end design for a restaurant website

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports={
     'secretKey':'12345-67890-09876-54321',
     'mongoUrl':'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '2416',
-        clientSecret: 'd791'
+        clientId: '2416',   //NOT CORRECT
+        clientSecret: 'd791'   //NOT CORRECT
     }
 };
